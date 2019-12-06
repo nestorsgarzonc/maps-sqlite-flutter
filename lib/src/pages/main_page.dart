84b7892faa25601eae79ef6ee239d80e7c1717ce
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2019-12-06 07:22:02 -0500

    first commit

---
+++ lib/src/pages/main_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:qrrecognitions_maps/src/pages/dirreciones_page.dart';
+import 'package:qrrecognitions_maps/src/pages/maps_page.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _page(currentIndex),
+      bottomNavigationBar: _bottonNavigationBar(),
+    );
+  }
+
+  Widget _page(int actualPage){
+    switch (actualPage) {
+      case 0: return MapsPage();
+      case 1: return DirreccionesPage();
+      default:
+        return MapsPage();
+    }
+  }
+
+  Widget _bottonNavigationBar(){
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index){
+        setState(() {
+          currentIndex=index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text("Maps")
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text("Dirrecciones")
+        ),
+      ],
+    );
+  }
+}
