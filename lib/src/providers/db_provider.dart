dc0ba52763f99c072a9baacbd818b070ea6513be
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2019-12-08 21:55:46 -0500

    create table and datatable

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class DBProvider {
   static Database _database;
@@ -11,5 +14,16 @@ class DBProvider {
     return _database;
   }
 
-  initDB() async {}
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('CREATE TABLA Scans ('
+          'id INTEGER PRIMARY KEY,'
+          'tipo TEXT,'
+          'valor TEXT'
+          ')');
+    });
+  }
 }
